5b43666a5f68138f02d256ba641977652ae5874f
T Heier <trentheier@gmail.com>
2021-05-20 18:11:42 -0400

    finished model

--- models/fitness.js
+++ models/fitness.js
@@ -3,21 +3,46 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fitnessData = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+  day: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+
+  workout: [{
+      type: {
+        type: String,
+        required: true,
+      },
+
+      name: {
+        type: String,
+        required: true,
+      },
+
+      duration: {
+        type: Number,
+        required: true,
+      },
+
+      weight: {
+        type: Number
+      },
+
+      reps: {
+        type: Number
+      },
+
+      sets: {
+        type: Number
+      },
+
+      distance: {
+        type: Number
+      },
+  }]
+
 });
 
-const fitnessData = mongoose.model("fitnessData", fitnessSchema);
+const fitnessData = mongoose.model("fitness", fitnessSchema);
 
 module.exports = fitnessData;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
+const Fitness = require("./models/fitness");
 
 const app = express();
 
